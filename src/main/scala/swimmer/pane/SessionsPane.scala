6d1194f646c469c68d566aa57fe74607bafe508f
objektwerks <objektwerks@runbox.com>
2025-08-03 08:28:33 -0400

    refactored session pane

--- src/main/scala/swimmer/pane/SessionsPane.scala
+++ src/main/scala/swimmer/pane/SessionsPane.scala
@@ -123,18 +123,18 @@ final class SessionsPane(context: Context, model: Model) extends VBox:
 
   def add(): Unit =
     SessionDialog(context, Session(swimmerId = model.selectedSwimmerId.value)).showAndWait() match
-      case Some(session: Session) => model.add(0, session) {
+      case Some(session: Session) =>
+        model.add(session)
         tableView.selectionModel().select(0)
-      }
       case _ =>
 
   def update(): Unit =
     val selectedIndex = tableView.selectionModel().getSelectedIndex
     val session = tableView.selectionModel().getSelectedItem.session
     SessionDialog(context, session).showAndWait() match
-      case Some(session: Session) => model.update(selectedIndex, session){
+      case Some(session: Session) =>
+        model.update(session)
         tableView.selectionModel().select(selectedIndex)
-      }
       case _ =>
 
   def chart(): Unit = ChartDialog(context, model).showAndWait()
