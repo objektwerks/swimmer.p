c16f104036792155776352cf9213264b2c89fbbd
objektwerks <objektwerks@runbox.com>
2025-08-05 10:00:33 -0400

    added store test

---
+++ src/main/test/scala/swimmer/StoreTest.scala
@@ -0,0 +1,18 @@
+import com.typesafe.config.ConfigFactory
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+final class StoreTest extends AnyFunSuite with Matchers:
+  val context = Context( ConfigFactory.load("test.conf") )
+  val store = Store(context)
+
+  var swimmer = Swimmer("fred")
+
+  test("store"):
+    store.addSwimmer(swimmer)
+
+    swimmer = swimmer.copy(name = "fred flintstone")
+    store.updateSwimmer(swimmer)
+
+    store.listSwimmers().length shouldBe 1
